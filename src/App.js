48fea575922b46e9d74a461ac6b47585e404fd30
Numan <numaniftikhar300@gmail.com>
2022-04-06 14:20:56 +0500

    added dark mode

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import PropTypes from 'prop-types'
-import TextForm from './components/TextForm';
+import About from './components/About';
+// import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils"/>
-      <TextForm heading="Enter your text below to analyse"/>
+      {/* <TextForm heading="Enter your text below to analyse"/> */}
+      <About/>
     </>
   );
 }
